53a9f39c86c2d5cba16eabe8eef7e08000826e24
khushmeetsheridan <157051046+khushmeetsheridan@users.noreply.github.com>
2024-10-11 15:37:24 -0400

    Add files via upload

---
+++ Books.sql
@@ -0,0 +1,11 @@
+INSERT INTO Books (title, author, ISBN , Genre) 
+VALUES('Chhah Vela', 'Kulwant singh Virk', '9780743273565', 'Fiction'),
+('A history of Sikhs', 'Khushwant singh', '9780451524935', 'Historical'),
+('Loona ', 'Shiv Kumar Batalvi', '9780060935467', 'Fiction'),
+('Birkh Arz Kare', 'Surjit Singh Patar', '9780316769488', 'Fiction'),
+('Tu main te kavita', 'Divnoor Buttar', '9781503280786', 'Romantic'),
+('Bhai Mardana', 'Bhai Vir Singh', '9780141040349', 'Romance'),
+('First Folio', 'William Shakespeare', '9780199232765', 'Fictional'),
+('The temple tulips', 'Puran Singh', '9780140268867', 'Fictional'),
+('Crime', 'Fyodor Dostoevsky', '9780143058144', 'Crime'),
+('Brothers Karamazov', 'Fyodor Dostoevsky', '9780140449242', 'Philosophical');
