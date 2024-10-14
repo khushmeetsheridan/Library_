96cbdacb1057cf8adefbee01f3d07314b21d5784
khushmeetsheridan <157051046+khushmeetsheridan@users.noreply.github.com>
2024-10-13 20:45:06 -0400

    Add files via upload

---
+++ borrowing_record.sql
@@ -0,0 +1,6 @@
+INSERT INTO borrowing_records (memberID, bookID, borrow_date, return_date)
+VALUES(1, 2, '2024-09-01', '2024-09-20'), 
+(3, 5, '2024-09-05', NULL), 
+(2, 8, '2024-09-10', '2024-09-25'), 
+(4, 3, '2024-09-15', NULL), 
+(5, 1, '2024-09-18', '2024-10-05');
