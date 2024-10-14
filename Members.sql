aafefaf8d8842dbf034ff392ee7fae58d1b0537f
khushmeetsheridan <157051046+khushmeetsheridan@users.noreply.github.com>
2024-10-13 20:43:58 -0400

    Update Members.sql

---
+++ Members.sql
@@ -0,0 +1,11 @@
+INSERT INTO Members (first_name, last_name, email, membership_date)
+values ('Khushmeet', 'Singh', 'khusmeet545@gmail.com', '2024-02-13'),
+('Om', 'Shivam', 'om2005@gmail.com', '2024-01-15'),
+('Ntish', 'Kumar', 'ntish@gmail.com', '2024-05-15'),
+('Akash', 'singh', 'akash@gmail.com', '2024-09-15'),
+('Anmol', 'Singh', 'anmol@gmail.com', '2024-09-15'),
+('Manjit', 'Maan', 'Maan2005@gmail.com', '2024-10-15'),
+('Prabh', 'Boparai', 'Boparai07@gmail.com', '2024-08-15'),
+('Daljit', 'Singh', 'daljit@gmail.com', '2024-07-15'),
+('Yuvraj', 'Singh', 'yash@gmail.com', '2024-06-15'),
+('Balkaran', 'Singh', 'balkaran@gmail.com', '2024-05-15');
